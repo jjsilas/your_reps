8fa0477933488b9ce7dff86c16ecb96400ee93a7
Jherich Silas <jherichsilas@Jherichs-MacBook-Pro-7.local>
2020-02-26 10:37:10 -0600

    Worked with Mario to sort out the api calls

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 'use strict';
 
-const apiKey = LUcpaYS4z2SvZUKWDsBRj7M6XrWbpVP5JBWMdAcW
+const apiKey = 'LUcpaYS4z2SvZUKWDsBRj7M6XrWbpVP5JBWMdAcW'
 
-const searchURL = 'https://api.propublica.org/congress/v1/members/{member-id}.json';
+const searchURL = 'https://api.propublica.org/congress/v1/116/senate/members.json';
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -29,16 +29,54 @@ function getNews(query, maxResults=10) {
 
   fetch(url, options)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => render(responseJson));
+}
+function render (data) {
+  const members = data.results[0].members
+  const html = members.map(member => `
+  <tr>
+    <td>${member.first_name} ${member.last_name}</td>
+    <td>${member.total_votes} (missed ${member.missed_votes}</td> 
+      <td> <button type="button" onClick="getVotes('${member.id}')"> getVotes </button> </td>
+  </tr>
+  `)
+  $('#vote').html(html)
+
 }
 
 function watchForm() {
-  $('form').submit(event => {
+  $('#js-form').submit(event => {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
     getNews(searchTerm, maxResults);
   });
+}
+function getVotes(member_id) {
+  
+  const url = `https://api.propublica.org/congress/v1/members/${member_id}/explanations/116.json`
+
+  console.log(url);
+  
+  const options = {
+    headers: new Headers({
+      "X-Api-Key": apiKey})
+  };
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(responseJson => renderVotes(responseJson));
+}
+function renderVotes (data) {
+  const votes = data.results
+  const html = votes.map(vote =>`
+  <tr>
+    <td>${vote.date}</td>
+    <td>${vote.text}</td> 
+      <td> ${vote.url} </td>
+  </tr>
+  `)
+  $('#vote').html(html)
 }
 
 $(watchForm);
